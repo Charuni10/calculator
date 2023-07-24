1769619b08c424a6b689e1cfa0febc0ee1e84715
Charuni SA <charuammu10@GMAIL.COM>
2023-07-24 19:33:22 +0530

    Initial commit

---
+++ calculator/src/App.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [result,setResult]=useState<string>("");
+   
+  const handleButton=(label: string):void=>{
+    switch (label ) {
+      case "AC":{
+        setResult("");
+        break;
+      }
+      case "DE":{
+        setResult((prevResult)=>prevResult.slice(0,-1));
+        break;
+      }
+      case "=":{
+        setResult((prevResult)=>eval(prevResult));
+        break;
+      }
+      default:{
+        setResult((prevResult) => prevResult + label);
+        
+      }
+    }
+  }
+  return (
+    <div className="container">
+      <div className='calc'>
+        <div id="disp" >
+          <p id='res' title={result}>{result}</p>
+        </div>
+        <div>
+        <button onClick={() => handleButton("AC")}>AC</button>
+        <button onClick={() => handleButton("DE")}>DE</button>
+        <button onClick={() => handleButton(".")}>.</button>
+        <button onClick={() => handleButton("/")}>/</button>
+        </div>
+        <div>
+        <button onClick={() => handleButton("7")}>7</button>
+        <button onClick={() => handleButton("8")}>8</button>
+        <button onClick={() => handleButton("9")}>9</button>
+        <button onClick={() => handleButton("*")}>*</button>
+        </div>
+        <div>
+        <button onClick={() => handleButton("4")}>4</button>
+        <button onClick={() => handleButton("5")}>5</button>
+        <button onClick={() => handleButton("6")}>6</button>
+        <button onClick={() => handleButton("-")}>-</button>
+        </div>
+        <div>
+        <button onClick={() => handleButton("1")}>1</button>
+        <button onClick={() => handleButton("2")}>2</button>
+        <button onClick={() => handleButton("3")}>3</button>
+        <button onClick={() => handleButton("+")}>+</button>
+        </div>
+        <div>
+        <button onClick={() => handleButton("00")}>00</button>
+        <button onClick={() => handleButton("0")}>0</button>
+        <button className='equal' onClick={() => handleButton("=")}>=</button>
+        
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
