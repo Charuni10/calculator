6a65ea772282f84e22b1c7a23878953e431a22da
Charuni SA <charuammu10@GMAIL.COM>
2023-07-25 12:30:19 +0530

    color-changed

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -25,42 +25,46 @@ function App() {
     }
   }
   return (
+    <div className='heading'>
+      <h1>Calculator</h1>
     <div className="container">
+    
       <div className='calc'>
         <div id="disp" >
           <p id='res' title={result}>{result}</p>
         </div>
-        <div>
-        <button onClick={() => handleButton("AC")}>AC</button>
-        <button onClick={() => handleButton("DE")}>DE</button>
-        <button onClick={() => handleButton(".")}>.</button>
-        <button onClick={() => handleButton("/")}>/</button>
+        <div >
+        <button  className='color' onClick={() => handleButton("AC")}>AC</button>
+        <button  className='color' onClick={() => handleButton("DE")}>DE</button>
+        <button  className='color' onClick={() => handleButton(".")}>.</button>
+        <button  className='color' onClick={() => handleButton("/")}>/</button>
         </div>
         <div>
         <button onClick={() => handleButton("7")}>7</button>
         <button onClick={() => handleButton("8")}>8</button>
         <button onClick={() => handleButton("9")}>9</button>
-        <button onClick={() => handleButton("*")}>*</button>
+        <button className='color' onClick={() => handleButton("*")}>*</button>
         </div>
         <div>
         <button onClick={() => handleButton("4")}>4</button>
         <button onClick={() => handleButton("5")}>5</button>
         <button onClick={() => handleButton("6")}>6</button>
-        <button onClick={() => handleButton("-")}>-</button>
+        <button className='color' onClick={() => handleButton("-")}>-</button>
         </div>
         <div>
         <button onClick={() => handleButton("1")}>1</button>
         <button onClick={() => handleButton("2")}>2</button>
         <button onClick={() => handleButton("3")}>3</button>
-        <button onClick={() => handleButton("+")}>+</button>
+        <button className='color' onClick={() => handleButton("+")}>+</button>
         </div>
         <div>
         <button onClick={() => handleButton("00")}>00</button>
         <button onClick={() => handleButton("0")}>0</button>
-        <button className='equal' onClick={() => handleButton("=")}>=</button>
+        <button className='equal color' onClick={() => handleButton("=")}>=</button>
         
         </div>
       </div>
+    </div>
     </div>
   );
 }
